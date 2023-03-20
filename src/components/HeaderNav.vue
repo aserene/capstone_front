c8bb7847d10b9ad34f89be5535510dec81e2e4f1
Athena Serene <aserenehoosang@gmail.com>
2023-03-20 14:51:24 -0700

    added header

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,51 @@
+<template>
+    <b-navbar>
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <h1>Pocket Journal</h1>
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="#">
+                <router-link to="/">Home</router-link>
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                <router-link to="/about">About</router-link>
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'HeaderNav'
+}
+</script>
+
+<style>
+nav{
+    width: 85%;
+    margin: 5px auto;
+}
+</style>
